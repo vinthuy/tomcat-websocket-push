6bcb0279d71fe0c5fb8870ce98ea02061965f46d
vinthuy <vinthuy@qq.com>
2017-07-03 00:17:07 +0800

    uptest

--- src/main/java/ws/Constants.java
+++ src/main/java/ws/Constants.java
@@ -5,8 +5,11 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.impl.LogFactoryImpl;
 
 /**
+ * 常量
  * Created by ruiyong.hry on 02/07/2017.
  */
 public class Constants {
+
     public static final Log wslogger = LogFactoryImpl.getLog(Constants.class);
+
 }
